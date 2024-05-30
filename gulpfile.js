7b9fc027126f8a4c4ca14c9bfc4f732eb76f30be
Brainner Blanco <brainnerblanco@gmail.com>
2024-05-30 11:57:14 -0600

    errores de deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,35 @@
-import { src, dest, watch, series } from 'gulp'
+import { src, dest, watch, series, parallel } from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 import terser from 'gulp-terser'
 
 const sass = gulpSass(dartSass)
 
-
-
 const paths = {
     scss: 'src/scss/**/*.scss',
     js: 'src/js/**/*.js'
 }
 
-export function css( done ) {
-    src(paths.scss, {sourcemaps: true})
-        .pipe( sass({
+export function css(done) {
+    src(paths.scss, { sourcemaps: true })
+        .pipe(sass({
             outputStyle: 'compressed'
-        }).on('error', sass.logError) )
-        .pipe( dest('./public/build/css', {sourcemaps: '.'}) );
+        }).on('error', sass.logError))
+        .pipe(dest('./public/build/css', { sourcemaps: '.' }))
     done()
 }
 
-export function js( done ) {
+export function js(done) {
     src(paths.js)
-      .pipe(terser())
-      .pipe(dest('./public/build/js'))
+        .pipe(terser())
+        .pipe(dest('./public/build/js'))
     done()
 }
 
 export function dev() {
-    watch( paths.scss, css );
-    watch( paths.js, js );
+    watch(paths.scss, css)
+    watch(paths.js, js)
 }
 
-export default series( js, css, dev )
+export const build = series(js, css)
+export default series(build, dev)
